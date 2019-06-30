223ce039a8969bcdac2f8a043440d23ae17c4abe
KuroyukiDev <kuroyuki.dev@gmail.com>
2019-06-30 10:07:58 -0700

    Initial PhoneGap app commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const displayContent = require('./display-content');
+
+
+window.onload = () => {
+	displayContent.appTitle();
+	displayContent.appBodyText();
+	displayContent.appAuthor();
+}
---
+++ src/vars.js
@@ -0,0 +1,12 @@
+
+const appAuthor = '[Your Name...]';
+
+let appBodyText = 'This is a Boilerplate App made in ES6 JS, jQuery Mobile, and HTML5.';
+
+let appTitle = 'App Title';
+
+module.exports = {
+	appAuthor: appAuthor,
+	appBodyText: appBodyText,
+	appTitle: appTitle
+};
